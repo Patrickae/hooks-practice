c46a6bb8d1e415b85511f4148dcece4c663db0a1
Andrew Patrick <andyepatrick@gmail.com>
2019-10-24 14:49:59 -0400

    context practice

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import LanguageSelector from './LanguageSelector';
+
+class App extends React.Component {
+	state={language: 'english'}
+
+	onLanguageChange = (language) => {
+		this.setState({language})
+	}
+
+	render(){
+		return (
+			<div className='ui container'>
+				<LanguageSelector onLanguageChange={this.onLanguageChange}/>
+				<LanguageContext.Provider value={this.state.language}>
+					<UserCreate />
+				</LanguageContext.Provider>
+			</div>
+		)
+	}
+}
+
+export default App
---
+++ src/components/LanguageSelector.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class LanguageSelector extends React.Component {
+	render() {
+		return (
+			<div>
+				Select a Language:
+				<i className='flag us' onClick={()=> this.props.onLanguageChange('english')}/>
+				<i className='flag nl' onClick={()=> this.props.onLanguageChange('dutch')}/>
+			</div>
+		);
+	}
+}
